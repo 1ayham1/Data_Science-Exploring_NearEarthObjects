3f349a0b444311e94f2646189b1649f1e412a6c5
me <learn.ayham@zoho.com>
2021-07-24 22:26:19 +0200

    basic csv impl.

--- write.py
+++ write.py
@@ -8,7 +8,6 @@ These functions are invoked by the main module with the output of the `limit`
 function and the filename supplied by the user at the command line. The file's
 extension determines which of these functions is used.
 
-You'll edit this file in Part 4.
 """
 import csv
 import json
@@ -25,7 +24,15 @@ def write_to_csv(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+    
+    with open(filename, "w") as out_stream:
+        writer = csv.DictWriter(out_stream,fieldnames=fieldnames)
+        writer.writeheader()
+        
+        for result in results:
+            writer.writerow(result)
+
+
 
 
 def write_to_json(results, filename):
