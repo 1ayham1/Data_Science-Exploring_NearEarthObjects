13e3855cdcac28452cddfe0df0c2f25071b4e796
me <learn.ayham@zoho.com>
2021-07-24 22:30:54 +0200

    skeleton for jason and csv

--- write.py
+++ write.py
@@ -46,4 +46,8 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    
+    with open(filename, 'w') as out_stream:
+        
+        for result in results:
+            json.dump(result,out_stream, indent=2)
