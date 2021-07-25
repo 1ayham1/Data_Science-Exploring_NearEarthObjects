739f3b7f51efe6f77e6e3ff8fa61fd8f760225d2
me <learn.ayham@zoho.com>
2021-07-25 20:59:16 +0200

    fixing style. line < 79

--- extract.py
+++ extract.py
@@ -1,4 +1,5 @@
-"""Extract data on near-Earth objects and close approaches from CSV and JSON files.
+"""Extract data on near-Earth objects and close approaches
+from CSV and JSON files.
 
 The `load_neos` function extracts NEO data from a CSV file, formatted as
 described in the project instructions, into a collection of `NearEarthObject`s.
@@ -7,8 +8,8 @@ The `load_approaches` function extracts close approach data from a JSON file,
 formatted as described in the project instructions, into a collection of
 `CloseApproach` objects.
 
-The main module calls these functions with the arguments provided at the command
-line, and uses the resulting collections to build an `NEODatabase`.
+The main module calls these functions with the arguments provided at
+the command line, and uses the resulting collections to build an `NEODatabase`.
 
 """
 import csv
@@ -16,10 +17,12 @@ import json
 
 from models import NearEarthObject, CloseApproach
 
+
 def load_neos(neo_csv_path):
     """Read near-Earth object information from a CSV file.
 
-    :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
+    :param neo_csv_path: A path to a CSV file containing data about
+    near-Earth objects.
     :return: A collection of `NearEarthObject`s.
     """
 
@@ -40,7 +43,8 @@ def load_neos(neo_csv_path):
 def load_approaches(cad_json_path):
     """Read close approach data from a JSON file.
 
-    :param neo_csv_path: A path to a JSON file containing data about close approaches.
+    :param neo_csv_path: A path to a JSON file containing data about
+    close approaches.
     :return: A collection of `CloseApproach`es.
     """
 
@@ -50,7 +54,9 @@ def load_approaches(cad_json_path):
 
         close_obj_keys = close_reader["fields"]
         close_obj_data = close_reader["data"]
-        close_reader_dict = [dict(zip(close_obj_keys, value)) for value in close_obj_data]
+        close_reader_dict = [
+            dict(zip(close_obj_keys, value))
+            for value in close_obj_data]
 
         close_approach_lst = []
 
